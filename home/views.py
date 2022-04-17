0e2c6fdc12e2bb7c25b3413c03002b2bcbdd59aa
davidkiama <davidkiama98@gmail.com>
2022-04-17 20:30:24 +0300

    Test refactored code

--- home/views.py
+++ home/views.py
@@ -13,17 +13,26 @@ def home(request):
     return render(request, 'index.html')
 
 
-def check_neighbourhood_obj(neigh_name, neigh_loc):
+def get_or_create_neighbourhood(request):
+
+    neigh_name = request.POST['neighbourhood_name']
+    neigh_loc = request.POST['neighbourhood_location']
 
     try:
         # Check if neighbourhood with passed in name and location exists
-        nei_obj = Neighbourhood.objects.filter(
+        neighbourhood = Neighbourhood.objects.filter(
             name=neigh_name, location=neigh_loc).first()
 
-        if nei_obj:
-            return nei_obj
+        if neighbourhood:
+            neighbourhood.update_occupants_count()
+            return neighbourhood
         else:
-            return False
+            # Create new neighbourhood
+            neighbourhood = Neighbourhood(
+                name=neigh_name, location=neigh_loc, admin=request.user, occupants_count=1)
+
+            neighbourhood.create_neighbourhood()
+            return neighbourhood
     except:
         return False
 
@@ -35,18 +44,7 @@ def setup_profile(request):
         user = request.user
         name = request.POST['full_names']
         email = request.POST['email']
-        neigh_name = request.POST['neighbourhood_name']
-        neigh_loc = request.POST['neighbourhood_location']
-
-        neighbourhood = check_neighbourhood_obj(neigh_name, neigh_loc)
-        if neighbourhood:
-            neighbourhood.update_occupants_count()
-        else:
-            # Create new neighbourhood
-            neighbourhood = Neighbourhood(
-                name=neigh_name, location=neigh_loc, admin=request.user, occupants_count=1)
-
-            neighbourhood.create_neighbourhood()
+        neighbourhood = get_or_create_neighbourhood(request)
 
         # Create profile
         profile = Profile(user=user, name=name, email=email,
